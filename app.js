bbd54a354db3ef05da3e7da1cee4d758492373d9
lorddxnowsy <beriaxtasy@gmail.com>
2020-06-16 18:41:12 +0400

    This one!

---
+++ app.js
@@ -0,0 +1,15 @@
+(function() {
+   "use strict";
+   
+
+    angular.module("myFirstApp", [])
+
+    .controller('MyFirstController', function ($scope) {
+        $scope.name = "Alex";
+        $scope.sayHello = function(){
+            return "Hello Fellas!";
+        }
+    
+    });
+
+})();
